c97b5064614fba53fb51384000ed96ffd7328b6e
JereSalo <jeresalo17@gmail.com>
2023-04-09 20:25:04 +0000

    borramos funciones innecesarias

--- shared/src/init.c
+++ shared/src/init.c
@@ -1,18 +1,7 @@
 #include "init.h"
 
-bool generar_conexiones(t_log* logger, int* socket_kernel) {
-    //LEVANTAR DEL ARCHIVO DE CONFIG IP Y PUERTO
 
-    t_config* config = config_create("consola.config");
-
-    char* ip_kernel = config_get_string_value(config, "IP_KERNEL");
-    printf(ip_kernel,"%s");
-    char* puerto_kernel = config_get_string_value(config, "PUERTO_KERNEL");
-
-    *socket_kernel = crear_conexion(logger, "KERNEL", ip_kernel, puerto_kernel);
-
-    return *socket_kernel != 0;
-}
+// generar Conexiones capaz debería recibir una ip y un puerto. Devolver la conexión 
 
 void cerrar_programa(t_log* logger) {
     log_destroy(logger);

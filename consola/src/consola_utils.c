d6b3f7923e736e2e9bc8fadd38d52260ff0561c2
JereSalo <jeresalo17@gmail.com>
2023-04-26 21:06:32 +0000

    cambio generar lista instrucciones

--- consola/src/consola_utils.c
+++ consola/src/consola_utils.c
@@ -35,6 +35,16 @@ long int calcular_tamanio_archivo(FILE * archivo){
 	return tamanio_archivo;
 }
 
+void liberar_lista(char** lista){
+    int contador = 0;
+    while(lista[contador] != NULL){
+        free(lista[contador]);
+        contador++;
+    }
+
+    free(lista);
+}
+
 t_list* generar_lista_instrucciones(char* archivo_string) {
     char** instrucciones = string_split(archivo_string, "\n");
     t_list* lista_instrucciones = list_create();
@@ -45,4 +55,29 @@ t_list* generar_lista_instrucciones(char* archivo_string) {
     }
 
     return lista_instrucciones;
+}
+
+t_list* generar_lista_instrucciones_2(char* path_instrucciones){
+    FILE* archivo_instrucciones = fopen(path_instrucciones, "r");
+    
+    if(archivo_instrucciones == NULL) {
+        printf("No se pudo abrir archivo de instrucciones ;)\n"); // Deberia ser un log, me gustaria crear un log aparte !
+    }
+    
+    t_list* instrucciones = list_create();
+
+    char linea[50];
+
+    while(fgets(linea,sizeof linea,archivo_instrucciones)!= NULL){
+        // En esta altura linea (a menos que sea la última) sería la instrucción mas el enter del final, quiero sacar el enter.
+        if(linea[strlen(linea)-1] == '\n'){linea[strlen(linea)-1] = '\0';} // Este if es nefasto pero bueno anda bien :)
+        list_add(instrucciones, strdup(linea)); // El strdup en sí leakea memoria, no es casi nada de memoria, no es importante pero bueno.
+    }
+
+
+    mostrar_lista(instrucciones);
+    //printf("%d",instrucciones->elements_count);
+    fclose(archivo_instrucciones);
+
+    return instrucciones;
 }

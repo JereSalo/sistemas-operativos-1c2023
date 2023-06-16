d12f52a79ce1a0da8e2d5d1f4fff9f83fd49167b
tleon <tleon@frba.utn.edu.ar>
2023-06-16 23:07:16 +0000

    Empezamos con la serializacion de segmentos, no termina de funcionar bien

--- shared/src/serializacion.c
+++ shared/src/serializacion.c
@@ -263,7 +263,7 @@ void deserializar_segmentos(void* stream, size_t size_segmentos , t_list* tabla_
 
 
 
-// ------------------------------ SERIALIZACION NUMERO (PRUEBA) ------------------------------ //
+// ------------------------------ SERIALIZACION NUMERITO (PRUEBA) ------------------------------ //
 
 void* serializar_numero(int numero) {
     

5978b08d0134fd2dbc9a7c3d60a1fd078a9eea95
tleon <tleon@frba.utn.edu.ar>
2023-07-27 23:30:42 +0000

    sucidio

--- consola/scripts/MEMORIA.sh
+++ consola/scripts/MEMORIA.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ../consola ../../tests/MEMORIA/MEMORIA_1 ../consola.config &
-sleep 1; ../consola ../../tests/MEMORIA/MEMORIA_2 ../consola.config &
-sleep 1; ../consola ../../tests/MEMORIA/MEMORIA_3 ../consola.config
+sleep 3; ../consola ../../tests/MEMORIA/MEMORIA_2 ../consola.config &
+sleep 3; ../consola ../../tests/MEMORIA/MEMORIA_3 ../consola.config
